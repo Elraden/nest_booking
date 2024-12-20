3fe8f443825f5ec436727efc4accc232099dd679
Elraden <morozovsa94@yandex.ru>
2024-12-20 09:05:56 +0300

    add services, add room dto's and controller

--- src/room/room.controller.ts
+++ src/room/room.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { CreateRoomDto } from './dto/create-room.dto';
+import { UpdateRoomDto } from './dto/update-room.dto';
 
 @Controller('room')
-export class RoomController {}
+export class RoomController { 
+
+	@Post('create')
+	create(@Body() createRoomDto: CreateRoomDto) { }
+
+	@Get('all')
+	findAll() {}
+
+	@Get(':id')
+	findById(@Param('id') id: string) {}
+
+	@Patch('update/:id')
+	update(@Param('id') id: string, @Body() updateRoomDto: UpdateRoomDto) {}
+
+	@Delete(':id')
+	delete(@Param('id') id: string) {}
+}
--- src/room/room.module.ts
+++ src/room/room.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { RoomController } from './room.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Room, RoomSchema } from './models/room.model';
+import { RoomService } from './room.service';
 
 @Module({
 	controllers: [RoomController],
-	imports: [MongooseModule.forFeature([{ name: Room.name, schema: RoomSchema}])]
+	imports: [MongooseModule.forFeature([{ name: Room.name, schema: RoomSchema}])],
+	providers: [RoomService]
 })
 export class RoomModule {}
