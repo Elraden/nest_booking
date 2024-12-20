e790aba39338c26b87b71cdbf2e15241e8d52cf0
Elraden <morozovsa94@yandex.ru>
2024-12-20 09:30:05 +0300

    added  created schedule dto and update controller

--- src/schedule/schedule.controller.ts
+++ src/schedule/schedule.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { CreateScheduleDto } from './dto/create-schedule.dto';
 
 @Controller('schedule')
-export class ScheduleController {}
+export class ScheduleController {
+	@Post('create')
+	async create(@Body() createScheduleDto: CreateScheduleDto) {}
+
+	@Get('all')
+	findAll() {}
+
+	@Get(':id')
+	findById(@Param('id') id: string) {}
+
+	@Delete('delete/:id')
+	delete(@Param('id') id: string) {}
+}
