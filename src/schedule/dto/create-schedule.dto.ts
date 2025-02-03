e99e79910228a6f50569735fbd6caf4fed434ac2
Elraden <morozovsa94@yandex.ru>
2025-02-03 16:44:55 +0300

    added validations

--- src/schedule/dto/create-schedule.dto.ts
+++ src/schedule/dto/create-schedule.dto.ts
@@ -1,4 +1,17 @@
+import { IsMongoId, IsISO8601, IsDefined } from 'class-validator';
+import {
+	ROOM_ID_INVALID,
+	ROOM_ID_REQUIRED,
+	DATE_INVALID,
+	DATE_REQUIRED,
+} from '../schedule.constants';
+
 export class CreateScheduleDto {
+	@IsMongoId({ message: ROOM_ID_INVALID })
+	@IsDefined({ message: ROOM_ID_REQUIRED })
 	roomId: string;
+
+	@IsISO8601({}, { message: DATE_INVALID })
+	@IsDefined({ message: DATE_REQUIRED })
 	date: string;
 }
